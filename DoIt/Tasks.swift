bf83573a967e763592d453cc235a715b35dfd68b
kanadenipun <nipunk@thoughtworks.com>
2017-07-14 09:36:32 +0530

    added table view for task list, added second screen for adding task, only navigation

---
+++ DoIt/Tasks.swift
@@ -0,0 +1,13 @@
+//
+// Created by NIPUN KANADE on 11/07/17.
+// Copyright (c) 2017 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+
+class Tasks {
+
+     var name = ""
+     var important = false
+
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet var tableView: UITableView!
+
+    var tasks : [Tasks] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+ 
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row];
+        if(task.important){
+            cell.textLabel?.text = "❗️\(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func makeTasks() -> [Tasks]{
+        let task1 = Tasks()
+        task1.name = "Walk the dog"
+        task1.important = false
+
+        let task2 = Tasks()
+        task2.name = "Buy Cheese"
+        task2.important = true
+
+        let task3 = Tasks()
+        task3.name = "Mow the new Lawn"
+        task3.important = false
+
+        return [task1, task2, task3]
+
     }
 
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
+        performSegue(withIdentifier: "addSegue", sender:nil)
+    }
 
 }
 
